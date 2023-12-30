a924155316680298d83fc7ec27f16b3695549af5
Danil Shcheblykin <catwiblues42@gmail.com>
2023-12-30 15:18:55 +0700

    add lesson1:task4

---
+++ task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 34;
+int b = 55;
+int c = 12;
+int d = 4;
+int e = 1;
+
+int max = a;
+
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+if(d > max)
+{
+    max = d;
+}
+if(e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
